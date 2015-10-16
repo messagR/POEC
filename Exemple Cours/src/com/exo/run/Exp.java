b02fb4e6faf45f99a86ce6e293bea9a865581a7c
PC <PC@123456789>
2015-10-16 17:04:04 +0200

    dfgswd

--- Exemple Cours/src/com/exo/run/Exp.java
+++ Exemple Cours/src/com/exo/run/Exp.java
@@ -1,8 +1,21 @@
 package com.exo.run;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
 public class Exp {
 
 	public static void main(String[] args) {
+
+		String test = "ttt;aaa;eee;uuu;ooo;sss;fff;hhh;kkk;mmm";
+		String test2 = "lksdjfjhndfqnflskd";
+		test.equalsIgnoreCase(test2);
+
 		Integer i = new Integer(25);
 		i = null;
 		// boxing
@@ -33,10 +46,53 @@ public class Exp {
 		Integer ai = new Integer(5);
 		double dd = ai.doubleValue();
 
-		Double dd2 = 5.135415874684 / 1.2346854;
-		System.out.println(dd2);
+		// TODO envoyer à christine
 
-		System.out.println();
+		Double dd2 = 00004254545.135415874684 / 1.2346854;
+		System.out.println(dd2);
+		DecimalFormat df = new DecimalFormat("#,###.##");
+		System.out.println(df.format(dd2));
+		DecimalFormat df1 = new DecimalFormat("0,000.00");
+		System.out.println(df1.format(dd2));
+
+		Locale l = Locale.getDefault();
+		System.out.println(l);
+		System.out.println(Locale.FRANCE);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date d2 = new Date();
+		System.out.println(d2);
+		System.out.println(sdf.format(d2));
+		String s1 = "2015/12/16 16:24:10";
+		Date d3 = null;
+		try {
+			d3 = sdf.parse(s1);
+			System.out.println(d3);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		String s2 = "2015/25/16 16:24:10";
+		try {
+			d3 = sdf.parse(s2);
+			System.out.println(d3);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		// pour ne pas que le format s'arrange pour ne pas planter
+		sdf.setLenient(false);
+		try {
+			d3 = sdf.parse(s2);
+			System.out.println(d3);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		Calendar c1 = new GregorianCalendar();
+		System.out.println(c1.getTime());
+		c1.set(Calendar.DAY_OF_YEAR, c1.get(Calendar.DAY_OF_YEAR) + 3);
+		System.out.println(c1.getTime());
+		System.out.println(c1.get(Calendar.DAY_OF_WEEK));
+
 	}
 
 }
